a845d443de8905320d2c0fb9869dc7cfa200e2a7
Chatmalow <maxime.pie.mail@gmail.com>
2020-04-08 10:58:16 +0200

    feat(prod): Add additional config for production build

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>EducHub</title>
-        <link rel="stylesheet" href="{{ asset('/css/app.css') }}"/>
+        <link rel="stylesheet" href="{{ secure_asset('/css/app.css') }}"/>
     </head>
     <body>
         <div id="root">
         </div>
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ secure_asset('js/app.js') }}"></script>
     </body>
 </html>
